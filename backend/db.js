e668c922da3dac0512d6169531453341e3f80a1c
d111784 <kunal.prabhakar@htdigital.in>
2024-04-14 13:00:12 +0530

    User schema added db

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+mongoose.connect("localhost:27017/wallet");
+const Userschema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model("User", Userschema);
+module.exports = {
+  User,
+};
